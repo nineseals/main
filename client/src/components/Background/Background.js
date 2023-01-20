cdaf6aab8749823f3c8baeca346412b946e5ac48
unfrgivn <bradley.ash@gmail.com>
2023-01-20 12:56:33 -0600

    fix: smoke bg on desktop

--- client/src/components/Background/Background.js
+++ client/src/components/Background/Background.js
@@ -5,18 +5,15 @@ import bgVideo from '../../assets/img/preloader-effect.mp4';
 import './Background.scss';
 
 const Background = (props) => {
-
     return (      
       <div id="qodef-page-spinner" className="qodef-m qodef-layout--plamen" style={{opacity: 1}}>
         <div className="qodef-m-inner">
           <div className="qodef-m-spinner">
             <div className="qodef-m-plamen">
-              <BrowserView>
-              <div className="qodef-m-plamen-smoke">
+              <BrowserView className="qodef-m-plamen-smoke">
                 <video className='videoTag' autoPlay loop muted>
                     <source src={bgVideo} type='video/mp4' />
                 </video>
-              </div>
               </BrowserView>
             </div> 
           </div>
