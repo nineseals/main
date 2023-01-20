0db1e1d572c260a9fbbde509fe7c712eaf064841
unfrgivn <bradley.ash@gmail.com>
2023-01-20 01:20:02 -0600

    Updates

---
+++ precache-manifest.1d328850f4b94145a4121d8c7cfd640e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b755e76073f2c243a4c9785c114e18",
+    "url": "/nineseals.github.io/index.html"
+  },
+  {
+    "revision": "5487f9117322d214d4ab",
+    "url": "/nineseals.github.io/static/css/2.a33d9fc9.chunk.css"
+  },
+  {
+    "revision": "46dd011b128ebcbcf8c7",
+    "url": "/nineseals.github.io/static/css/main.404966b4.chunk.css"
+  },
+  {
+    "revision": "5487f9117322d214d4ab",
+    "url": "/nineseals.github.io/static/js/2.6f2c86d5.chunk.js"
+  },
+  {
+    "revision": "46dd011b128ebcbcf8c7",
+    "url": "/nineseals.github.io/static/js/main.bcbbf83c.chunk.js"
+  },
+  {
+    "revision": "b7210f9b9a61442cd421",
+    "url": "/nineseals.github.io/static/js/runtime-main.f0ddba36.js"
+  },
+  {
+    "revision": "66ebe83846948512e27eafd6963ec7e6",
+    "url": "/nineseals.github.io/static/media/demonic_9_logo.66ebe838.jpg"
+  },
+  {
+    "revision": "cfd485a8368d5c093f8374ee7074a2ef",
+    "url": "/nineseals.github.io/static/media/nine_seals_1.cfd485a8.png"
+  },
+  {
+    "revision": "8beb71c69efd2813f8efa03dce6ebfe1",
+    "url": "/nineseals.github.io/static/media/nine_seals_2.8beb71c6.png"
+  },
+  {
+    "revision": "b66560b2353ac48196f0c98b9587a102",
+    "url": "/nineseals.github.io/static/media/nine_seals_3.b66560b2.png"
+  },
+  {
+    "revision": "7f9037306ce5cd8265a7196fa9b8ce74",
+    "url": "/nineseals.github.io/static/media/nine_seals_4.7f903730.png"
+  },
+  {
+    "revision": "14e02e67774e0b029cc7950608d74e5c",
+    "url": "/nineseals.github.io/static/media/nine_seals_5.14e02e67.png"
+  },
+  {
+    "revision": "e497661af7a65d44c0fa789d920bedc7",
+    "url": "/nineseals.github.io/static/media/nine_seals_6.e497661a.png"
+  },
+  {
+    "revision": "26e537ac67a37fa2eed98b329d63bcb5",
+    "url": "/nineseals.github.io/static/media/nine_seals_7.26e537ac.png"
+  },
+  {
+    "revision": "546aa2bd69d28d408812bf8fdc53073e",
+    "url": "/nineseals.github.io/static/media/nine_seals_8.546aa2bd.png"
+  },
+  {
+    "revision": "dbc96a312863f7022977e0c80a56d384",
+    "url": "/nineseals.github.io/static/media/nine_seals_9.dbc96a31.png"
+  },
+  {
+    "revision": "26e537ac67a37fa2eed98b329d63bcb5",
+    "url": "/nineseals.github.io/static/media/pfp_artist.26e537ac.png"
+  },
+  {
+    "revision": "e14ff18f998beb94f9b9c91c7eedd022",
+    "url": "/nineseals.github.io/static/media/pfp_ikari.e14ff18f.png"
+  },
+  {
+    "revision": "4cab6a0ac64e7e522fce7d3ca3c24264",
+    "url": "/nineseals.github.io/static/media/pfp_itsbradleybitch.4cab6a0a.png"
+  },
+  {
+    "revision": "1665a85063b9d9f2f8406c6148493b97",
+    "url": "/nineseals.github.io/static/media/preloader-background.1665a850.jpg"
+  },
+  {
+    "revision": "f3af0cd000a73e3a050ce907e6d2dbbf",
+    "url": "/nineseals.github.io/static/media/preloader-effect.f3af0cd0.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/main/precache-manifest.5fbb6651cdf740a7cf1d54af44c4f074.js"
+  "/nineseals.github.io/precache-manifest.1d328850f4b94145a4121d8c7cfd640e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/main/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nineseals.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
