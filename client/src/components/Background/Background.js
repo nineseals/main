b851c1c1ea11e0030ed90ca5699103db2f12441e
unfrgivn <bradley.ash@gmail.com>
2023-01-20 12:50:22 -0600

    fix: mobile styles

--- client/src/components/Background/Background.js
+++ client/src/components/Background/Background.js
@@ -6,22 +6,22 @@ import './Background.scss';
 
 const Background = (props) => {
 
-    return (
-      <BrowserView>
+    return (      
       <div id="qodef-page-spinner" className="qodef-m qodef-layout--plamen" style={{opacity: 1}}>
         <div className="qodef-m-inner">
           <div className="qodef-m-spinner">
             <div className="qodef-m-plamen">
+              <BrowserView>
               <div className="qodef-m-plamen-smoke">
                 <video className='videoTag' autoPlay loop muted>
                     <source src={bgVideo} type='video/mp4' />
                 </video>
               </div>
+              </BrowserView>
             </div> 
           </div>
         </div>
-      </div>
-      </BrowserView>
+      </div>      
     );
 }
 
