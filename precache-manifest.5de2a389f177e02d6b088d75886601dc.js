eb90b2ad4a8c7dbf849275b44959331ff65021f8
unfrgivn <bradley.ash@gmail.com>
2023-01-22 12:47:34 -0600

    Updates

--- precache-manifest.5de2a389f177e02d6b088d75886601dc.js
+++ precache-manifest.5de2a389f177e02d6b088d75886601dc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a7c0015484ca0ad5ac13c13bc944392c",
+    "revision": "c7a3717c7376d2ac841011d50a4d0a00",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.a33d9fc9.chunk.css"
   },
   {
-    "revision": "1b5761343b5b87cca9bd",
+    "revision": "d4fee087df17509c50ff",
     "url": "/static/css/main.2aa956dc.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.272794dc.chunk.js"
   },
   {
-    "revision": "1b5761343b5b87cca9bd",
-    "url": "/static/js/main.b1b4d753.chunk.js"
+    "revision": "d4fee087df17509c50ff",
+    "url": "/static/js/main.02f6afa5.chunk.js"
   },
   {
     "revision": "0a31ddf7382e0363a369",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6179a6e4ed5bc2fe7a5cc88c73d7b90.js"
+  "/precache-manifest.5de2a389f177e02d6b088d75886601dc.js"
 );
 
 self.addEventListener('message', (event) => {
